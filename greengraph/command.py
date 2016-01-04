83f7e1e695b8fdb9052bf9b0ad438d67dbda0818
Danial Dervovic <dannydervovic@hotmail.com>
2016-01-04 20:24:49 +0000

    Fixed command line help message

--- greengraph/command.py
+++ greengraph/command.py
@@ -7,7 +7,7 @@ def process():
     parser = ArgumentParser(description="Plots amount of green space between two locations.")
     parser.add_argument('start', help='starting location, string')
     parser.add_argument('end', help='final location, string')
-    parser.add_argument('--steps', type=int, default=20, help='number of calculated points, defaults to 20')
+    parser.add_argument('--steps', type=int, default=20, help='number of calculated points, defaults to 20, integer')
     parser.add_argument('--out', help='output file, "*.png" or "*.pdf"', default=False)
     arguments = parser.parse_args()
 
